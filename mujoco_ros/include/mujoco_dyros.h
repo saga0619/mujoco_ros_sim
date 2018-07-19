87359049f0c0abccc13907380ae35b6c2c1c8cf1
saga702 <june992@gmail.com>
2018-07-19 11:32:37 +0900

    v1.0

---
+++ mujoco_ros/include/mujoco_dyros.h
@@ -0,0 +1,300 @@
+#ifndef _MUJOCO_DYROS_H
+#define _MUJOCO_DYROS_H
+
+
+#include <ros/ros.h>
+#include <Eigen/Dense>
+#include <thread>
+#include <mujoco.h>
+#include <stdio.h>
+#include <glfw3.h>
+#include <string.h>
+
+#include <mujoco_ros_msgs/JointState.h>
+#include <mujoco_ros_msgs/SensorState.h>
+#include <mujoco_ros_msgs/JointSet.h>
+#include <std_msgs/String.h>
+
+//-------------------------------- global variables -------------------------------------
+
+// model
+mjModel* m = 0;
+mjData* d = 0;
+char lastfile[1000] = "";
+
+
+
+// user state
+bool paused = true;
+bool showoption = false;
+bool showinfo = true;
+bool showfullscreen = false;
+bool slowmotion = false;
+bool showdepth = false;
+bool showsensor = false;
+bool showprofiler = true;
+bool showdebug = false;
+bool showfixcam = false;
+int showhelp = 1;                   // 0: none; 1: brief; 2: full
+int fontscale = mjFONTSCALE_150;    // can be 100, 150, 200
+int keyreset = 0;                  // non-negative: reset to keyframe
+
+// abstract visualization
+mjvScene scn;
+mjvCamera cam;
+mjvOption vopt;
+mjvPerturb pert;
+mjvFigure figconstraint;
+mjvFigure figcost;
+mjvFigure figtimer;
+mjvFigure figsize;
+mjvFigure figsensor;
+char status[1000] = "";
+
+// OpenGL rendering
+int refreshrate;
+mjrContext con;
+float depth_buffer[5120*2880];        // big enough for 5K screen
+unsigned char depth_rgb[1280*720*3];  // 1/4th of screen
+
+// For additional window
+mjvScene scn2;
+mjvCamera cam2;
+mjrContext con2;
+
+
+
+
+// selection and perturbation
+bool button_left = false;
+bool button_middle = false;
+bool button_right =  false;
+double lastx = 0;
+double lasty = 0;
+double window2buffer = 1;           // framebuffersize / windowsize (for scaled video modes)
+
+// help strings
+const char help_title[] =
+  "Help\n"
+  "Option\n"
+  "Info\n"
+  "Depth\n"
+  "Full screen\n"
+  "Stereo\n"
+  "Sensor\n"
+"Profiler\n"
+  "Debug\n"
+  "Fixcam\n"
+"Slow motion\n"
+"Key reset\n"
+"Pause\n"
+"Reset\n"
+"Forward\n"
+"Back\n"
+"Forward 100\n"
+"Back 100\n"
+"Autoscale\n"
+"Reload\n"
+"Geoms\n"
+"Sites\n"
+"Select\n"
+"Center\n"
+"Track\n"
+"Zoom\n"
+"Translate\n"
+"Rotate\n"
+"Perturb\n"
+"Free Camera\n"
+"Camera\n"
+"Frame\n"
+"Label\n"
+"Fontsize";
+
+
+const char help_content[] =
+"F1\n"
+"F2\n"
+"F3\n"
+"F4\n"
+"F5\n"
+"F6\n"
+"F7\n"
+"F8\n"
+"F9\n"
+"F10\n"
+"Enter\n"
+"Page Up/Down\n"
+"Space\n"
+"BackSpace\n"
+"Right arrow\n"
+"Left arrow\n"
+"Down arrow\n"
+"Up arrow\n"
+"Ctrl A\n"
+"Ctrl L\n"
+"0 - 4\n"
+"Shift 0 - 4\n"
+"L dblclick\n"
+"R dblclick\n"
+"Ctrl R dblclick\n"
+"Scroll or M drag\n"
+"[Shift] R drag\n"
+"L drag\n"
+"Ctrl [Shift] L/R drag\n"
+"Esc\n"
+"[ ]\n"
+"; '\n"
+". /\n"
+"- =";
+
+char opt_title[1000] = "";
+char opt_content[1000];
+
+//---------------ROS ----------
+ros::Publisher joint_state_pub;
+ros::Publisher sensor_state_pub;
+ros::Subscriber joint_set;
+ros::Subscriber joint_init;
+ros::Subscriber sim_command_sub;
+ros::Publisher sim_command_pub;
+mujoco_ros_msgs::JointState joint_state_msg_;
+mujoco_ros_msgs::JointSet joint_set_msg_;
+mujoco_ros_msgs::SensorState sensor_state_msg_;
+
+
+
+bool ros_time_sync_reset;
+
+//reset start time
+bool ros_sim_started = true;
+
+
+
+
+ros::Time ros_sim_starttm;
+ros::Duration ros_time_paused;
+
+ros::Duration ros_sim_runtime;
+ros::Time ros_time_paused_starttm;
+ros::Time ros_time_paused_stoptm;
+ros::Time ros_time_sm_starttm;
+ros::Time ros_time_sm_stoptm;
+
+
+
+void mujoco_ros_connector_init();
+
+
+
+
+
+int timesync_count=0;
+double timesync_mean=0;
+
+
+mjtNum* torque_mj;
+
+
+
+//-------------------------------- profiler and sensor ----------------------------------
+
+// init profiler
+void profilerinit(void);
+
+// show profiler
+void profilerupdate(void);
+
+// show profiler
+void profilershow(mjrRect rect);
+
+// init sensor figure
+void sensorinit(void);
+
+// update sensor figure
+void sensorupdate(void);
+
+// show sensor figure
+void sensorshow(mjrRect rect);
+
+
+
+
+//-------------------------------- utility functions ------------------------------------
+
+// center and scale view
+void autoscale(GLFWwindow* window);
+
+// load mjb or xml model
+void loadmodel(GLFWwindow* window, const char* filename);
+
+// timer in milliseconds
+mjtNum timer(void);
+
+// clear all times
+void cleartimers(mjData* d);
+
+
+
+
+//--------------------------------- GLFW callbacks --------------------------------------
+
+// keyboard
+void keyboard(GLFWwindow* window, int key, int scancode, int act, int mods);
+
+// mouse button
+void mouse_button(GLFWwindow* window, int button, int act, int mods);
+
+// mouse move
+void mouse_move(GLFWwindow* window, double xpos, double ypos);
+
+// scroll
+void scroll(GLFWwindow* window, double xoffset, double yoffset);
+
+// drop
+void drop(GLFWwindow* window, int count, const char** paths);
+
+
+
+
+
+
+//-------------------------------- simulation and rendering -----------------------------
+
+// make option string
+void makeoptionstring(const char* name, char key, char* buf);
+
+// advance simulation
+void simulation(void);
+
+// render
+void render(GLFWwindow* window);
+
+void render_depth(GLFWwindow* main_window, GLFWwindow* sub_window);
+
+
+//-------------------------------- user created controller ---------------------------//
+
+void mycontroller(const mjModel* m, mjData* d);
+
+void mycontrollerinit();
+
+void state_publisher(const mjModel* m, mjData* d);
+
+void state_publisher_init(const mjModel* m, mjData* d);
+
+void sensor_callback(const mjModel* m, mjData* d, int num);
+
+void jointset_callback(const mujoco_ros_msgs::JointSetConstPtr& msg);
+
+void jointinit_callback(const mujoco_ros_msgs::JointStateConstPtr& msg);
+
+void sim_command_callback(const std_msgs::StringConstPtr& msg);
+
+
+
+
+//-------------------------------- little math works --------------//
+
+
+
+#endif
---
+++ mujoco_ros/src/main.cpp
@@ -0,0 +1,151 @@
+#include "mujoco_dyros.h"
+
+
+
+
+
+
+
+
+//-------------------------------- main function ----------------------------------------
+
+int main(int argc, char** argv)
+{
+    //ros init
+    ros::init(argc, argv, "mujoco");
+    ros::NodeHandle nh("~");
+    std::string key_file;
+    nh.param<std::string>("license", key_file, "mjkey.txt");
+
+    // print version, check compatibility
+    printf("MuJoCo Pro library version %.2lf\n", 0.01*mj_version());
+    if( mjVERSION_HEADER!=mj_version() )
+        mju_error("Headers and library have different versions");
+
+
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+        ROS_INFO_COND(false,"Current working dir: %s\n", cwd);
+
+    ROS_INFO("license is at %s", key_file.c_str());
+    // activate MuJoCo license
+    // locate mjkey.txt file at /home/<usrname>
+    mj_activate(key_file.c_str());
+
+    // init GLFW
+    if (!glfwInit())
+        return 1;
+
+    // get refreshrate
+    refreshrate = glfwGetVideoMode(glfwGetPrimaryMonitor())->refreshRate;
+
+    // multisampling
+    glfwWindowHint(GLFW_SAMPLES, 4);
+
+    // create widdow
+    GLFWwindow* window = glfwCreateWindow(1200, 900, "Simulate", NULL, NULL);
+    GLFWwindow* window2;// = glfwCreateWindow(800, 600, "Sub camera", NULL, NULL);
+    if( !window )
+    {
+        glfwTerminate();
+        return 1;
+    }
+    if( !window2 )
+    {
+      glfwTerminate();
+      return 1;
+    }
+
+    // make context current, disable v-sync
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    // save window-to-framebuffer pixel scaling (needed for OSX scaling)
+    int width, width1, height;
+    glfwGetWindowSize(window, &width, &height);
+    glfwGetFramebufferSize(window, &width1, &height);
+    window2buffer = (double)width1 / (double)width;
+
+    // init MuJoCo rendering, get OpenGL info
+    mjv_makeScene(&scn, 1000);
+    mjv_defaultCamera(&cam);
+    mjv_defaultOption(&vopt);
+    mjr_defaultContext(&con);
+    mjr_makeContext(m, &con, fontscale);
+    profilerinit();
+    sensorinit();
+
+    // Second display for fixed cam, but not working. why?//
+    /*
+    glfwMakeContextCurrent(window2);
+    mjv_makeScene(&scn2, 1000);
+    mjv_defaultCamera(&cam2);
+    mjr_defaultContext(&con2);
+    mjr_makeContext(m,&con2,fontscale);
+    */
+
+    glfwMakeContextCurrent(window);
+    // set GLFW callbacks
+    glfwSetKeyCallback(window, keyboard);
+    glfwSetCursorPosCallback(window, mouse_move);
+    glfwSetMouseButtonCallback(window, mouse_button);
+    glfwSetScrollCallback(window, scroll);
+    glfwSetDropCallback(window, drop);
+    glfwSetWindowRefreshCallback(window, render);
+
+
+
+    // set MuJoCo time callback for profiling
+    mjcb_time = timer;
+
+    // load model if filename given as argument
+    if( argc==2 )
+        loadmodel(window, argv[2]);
+
+
+    std::string model_file;
+    if(nh.getParam("model_file",model_file))
+        loadmodel(window,model_file.c_str());
+ROS_INFO("model is at %s", model_file.c_str());
+
+    //register callback function//
+    mjcb_control=mycontroller;
+    mjcb_sensor=sensor_callback;
+
+
+
+
+    //register publisher & subscriber
+    joint_state_pub = nh.advertise<mujoco_ros_msgs::JointState>("/mujoco_ros_interface/joint_states", 1);
+    sensor_state_pub = nh.advertise<mujoco_ros_msgs::SensorState>("/mujoco_ros_interface/sensor_states",1);
+    joint_set = nh.subscribe<mujoco_ros_msgs::JointSet>("/mujoco_ros_interface/joint_set",1,jointset_callback,ros::TransportHints().tcpNoDelay(true));
+    joint_init = nh.subscribe("/mujoco_ros_interface/joint_init",100,jointinit_callback);
+    sim_command_sub = nh.subscribe("/mujoco_ros_interface/sim_command_con2sim",100,sim_command_callback);
+    sim_command_pub = nh.advertise<std_msgs::String>("/mujoco_ros_interface/sim_command_sim2con",1);
+
+    // main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // simulate and render
+        render(window);
+        //render_depth(window, window2);
+
+        // handle events (this calls all callbacks)
+        glfwPollEvents();
+    }
+
+    // delete everything we allocated
+    mj_deleteData(d);
+    mj_deleteModel(m);
+    mjr_freeContext(&con);
+    mjv_freeScene(&scn);
+    mjr_freeContext(&con2);
+    mjv_freeScene(&scn2);
+
+    // terminate
+    glfwTerminate();
+    mj_deactivate();
+    return 0;
+}
+
+
