20eeffe371a1f4448ad5462273fb9af1fb159eeb
junhee Ahn <june992@gmail.com>
2019-01-07 18:14:27 +0900

    2.0

---
+++ mujoco_2_ros/include/mjros.h
@@ -0,0 +1,317 @@
+#ifndef _MJROS_H
+#define _MJROS_H
+
+//Mujoco include
+#include "mjxmacro.h"
+#include "uitools.h"
+#include "stdio.h"
+#include "string.h"
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+//Ros include
+#include <ros/ros.h>
+#include <Eigen/Dense>
+#include <vector>
+#include <mujoco_ros_msgs/SensorState.h>
+#include <mujoco_ros_msgs/JointSet.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Float32.h>
+#include <sensor_msgs/JointState.h>
+#include <tf/transform_datatypes.h>
+
+//-------------------------------- global -----------------------------------------------
+
+//-----mujoco var-----
+// constants
+const int maxgeom = 5000;         // preallocated geom array in mjvScene
+const double syncmisalign = 0.1;  // maximum time mis-alignment before re-sync
+const double refreshfactor = 0.7; // fraction of refresh available for simulation
+
+// model and data
+mjModel *m = NULL;
+mjData *d = NULL;
+char filename[1000] = "";
+
+// abstract visualization
+mjvScene scn;
+mjvCamera cam;
+mjvOption vopt;
+mjvPerturb pert;
+mjvFigure figconstraint;
+mjvFigure figcost;
+mjvFigure figtimer;
+mjvFigure figsize;
+mjvFigure figsensor;
+
+// OpenGL rendering and UI
+GLFWvidmode vmode;
+int windowpos[2];
+int windowsize[2];
+mjrContext con;
+GLFWwindow *window = NULL;
+mjuiState uistate;
+mjUI ui0, ui1;
+
+int key_ui = 0;
+
+// UI settings not contained in MuJoCo structures
+struct setting_
+{
+    // file
+    int exitrequest = 0;
+
+    // option
+    int spacing = 0;
+    int color = 0;
+    int font = 0;
+    int ui0 = 1;
+    int ui1 = 0;
+    int help = 0;
+    int info = 1;
+    int profiler = 0;
+    int sensor = 0;
+    int fullscreen = 0;
+    int vsync = 1;
+    int busywait = 0;
+    int debug = 0;
+    int testbtn2 = 1;
+
+    // simulation
+    int run = 0;
+    int key = 0;
+    char key_s[40] = "0";
+    int loadrequest = 0;
+
+    // watch
+    char field[mjMAXUITEXT] = "qpos";
+    int index = 0;
+
+    // physics: need sync
+    int disable[mjNDISABLE];
+    int enable[mjNENABLE];
+
+    // rendering: need sync
+    int camera = 0;
+};
+
+setting_ settings;
+
+// section ids
+enum
+{
+    // left ui
+    SECT_FILE = 0,
+    SECT_OPTION,
+    SECT_SIMULATION,
+    SECT_PHYSICS,
+    SECT_RENDERING,
+    SECT_GROUP,
+    NSECT0,
+
+    // right ui
+    SECT_WATCH = 0,
+    SECT_JOINT,
+    SECT_CONTROL,
+    NSECT1
+};
+
+// file section of UI
+const mjuiDef defFile[] =
+    {
+        {mjITEM_SECTION, "File", 1, NULL, "AF"},
+        {mjITEM_BUTTON, "Save xml", 2, NULL, ""},
+        {mjITEM_BUTTON, "Save mjb", 2, NULL, ""},
+        {mjITEM_BUTTON, "Print model", 2, NULL, "CM"},
+        {mjITEM_BUTTON, "Print data", 2, NULL, "CD"},
+        {mjITEM_BUTTON, "Quit", 1, NULL, "CQ"},
+        {mjITEM_END}};
+
+// option section of UI
+const mjuiDef defOption[] =
+    {
+        {mjITEM_SECTION, "Option", 1, NULL, "AO"},
+        {mjITEM_SELECT, "Spacing", 1, &settings.spacing, "Tight\nWide"},
+        {mjITEM_SELECT, "Color", 1, &settings.color, "Default\nOrange\nWhite\nBlack"},
+        {mjITEM_SELECT, "Font", 1, &settings.font, "50 %\n100 %\n150 %\n200 %\n250 %\n300 %"},
+        {mjITEM_CHECKINT, "Left UI (Tab)", 1, &settings.ui0, " #258"},
+        {mjITEM_CHECKINT, "Right UI", 1, &settings.ui1, "S#258"},
+        {mjITEM_CHECKINT, "Help", 2, &settings.help, " #290"},
+        {mjITEM_CHECKINT, "Info", 2, &settings.info, " #291"},
+        {mjITEM_CHECKINT, "Profiler", 2, &settings.profiler, " #292"},
+        {mjITEM_CHECKINT, "Sensor", 2, &settings.sensor, " #293"},
+#ifdef __APPLE__
+        {mjITEM_CHECKINT, "Fullscreen", 0, &settings.fullscreen, " #294"},
+#else
+        {mjITEM_CHECKINT, "Fullscreen", 1, &settings.fullscreen, " #294"},
+#endif
+        {mjITEM_CHECKINT, "Vertical Sync", 1, &settings.vsync, " #295"},
+        {mjITEM_CHECKINT, "Busy Wait", 1, &settings.busywait, " #296"},
+        {mjITEM_CHECKINT, "Debug", 1, &settings.debug, " #297"},
+        {mjITEM_CHECKINT, "TEST btn2", 1, &settings.testbtn2, " #298"},
+        {mjITEM_END}};
+
+// simulation section of UI
+mjuiDef defSimulation[] =
+    {
+        {mjITEM_SECTION, "Simulation", 1, NULL, "AS"},
+        {mjITEM_RADIO, "", 2, &settings.run, "Pause\nRun"},
+        {mjITEM_BUTTON, "Reset", 2, NULL, "C#259"},
+        {mjITEM_BUTTON, "Reload", 2, NULL, "CL"},
+        {mjITEM_BUTTON, "Align", 2, NULL, "CA"},
+        {mjITEM_BUTTON, "Copy pose", 2, NULL, "CC"},
+        //{mjITEM_SLIDERINT, "Key", 3, &settings.key, "0 0"},
+        {mjITEM_BUTTON, "Key + ", 2, NULL, " #266"},
+        {mjITEM_BUTTON, "Key - ", 2, NULL, " #267"},
+        {mjITEM_STATIC, "Key", 2, NULL, " 0"},
+        {mjITEM_BUTTON, "Reset to key", 3, NULL, " #259"},
+        {mjITEM_BUTTON, "Set key", 3},
+        {mjITEM_END}};
+
+// watch section of UI
+const mjuiDef defWatch[] =
+    {
+        {mjITEM_SECTION, "Watch", 0, NULL, "AW"},
+        {mjITEM_EDITTXT, "Field", 2, settings.field, "qpos"},
+        {mjITEM_EDITINT, "Index", 2, &settings.index, "1"},
+        {mjITEM_STATIC, "Value", 2, NULL, " "},
+        {mjITEM_END}};
+
+// help strings
+const char help_content[] =
+    "Alt mouse button\n"
+    "UI right hold\n"
+    "UI title double-click\n"
+    "Space\n"
+    "Esc\n"
+    "Right arrow\n"
+    "Left arrow\n"
+    "Down arrow\n"
+    "Up arrow\n"
+    "Page Up\n"
+    "Double-click\n"
+    "Right double-click\n"
+    "Ctrl Right double-click\n"
+    "Scroll, middle drag\n"
+    "Left drag\n"
+    "[Shift] right drag\n"
+    "Ctrl [Shift] drag\n"
+    "Ctrl [Shift] right drag";
+
+const char help_title[] =
+    "Swap left-right\n"
+    "Show UI shortcuts\n"
+    "Expand/collapse all  \n"
+    "Pause\n"
+    "Free camera\n"
+    "Step forward\n"
+    "Step back\n"
+    "Step forward 100\n"
+    "Step back 100\n"
+    "Select parent\n"
+    "Select\n"
+    "Center\n"
+    "Track camera\n"
+    "Zoom\n"
+    "View rotate\n"
+    "View translate\n"
+    "Object rotate\n"
+    "Object translate";
+
+// info strings
+char info_title[1000];
+char info_content[1000];
+
+void profilerinit(void);
+void profilerupdate(void);
+void profilershow(mjrRect rect);
+void sensorinit(void);
+void sensorupdate(void);
+void sensorshow(mjrRect rect);
+void infotext(char *title, char *content, double interval);
+void printfield(char *str, void *ptr);
+void watch(void);
+void makephysics(int oldstate);
+void makerendering(int oldstate);
+void makegroup(int oldstate);
+void makejoint(int oldstate);
+void makecontrol(int oldstate);
+void makesections(void);
+void alignscale(void);
+void copykey(void);
+mjtNum timer(void);
+void cleartimers(void);
+void updatesettings(void);
+void drop(GLFWwindow *window, int count, const char **paths);
+void loadmodel(void);
+int uiPredicate(int category, void *userdata);
+void uiLayout(mjuiState *state);
+void uiEvent(mjuiState *state);
+void prepare(void);
+void render(GLFWwindow *window);
+void simulate(void);
+void init(std::string key_file);
+void rosPollEvents();
+
+std::mutex mtx;
+
+//---------------ROS Var-----------------------
+ros::Publisher joint_state_pub;
+ros::Publisher sensor_state_pub;
+ros::Subscriber joint_set;
+ros::Subscriber joint_init;
+ros::Subscriber sim_command_sub;
+ros::Publisher sim_command_pub;
+
+//mujoco_ros_msgs::JointState joint_state_msg_;
+//mujoco_ros_msgs::JointSet joint_set_msg_;
+mujoco_ros_msgs::SensorState sensor_state_msg_;
+sensor_msgs::JointState joint_state_msg_;
+//sensor_msgs::JointState joint_set_msg_;
+mujoco_ros_msgs::JointSet joint_set_msg_;
+std_msgs::Float32 sim_time;
+ros::Publisher sim_time_pub;
+
+std::vector<double> command;
+
+int loadmodel_request = 0;
+
+bool ros_time_sync_reset;
+
+//reset start time
+bool ros_sim_started = true;
+bool controller_reset_check = true;
+bool controller_init_check = true;
+bool reset_request = false;
+
+ros::Time ros_sim_starttm;
+ros::Duration ros_time_paused;
+
+ros::Duration ros_sim_runtime;
+ros::Time ros_time_paused_starttm;
+ros::Time ros_time_paused_stoptm;
+ros::Time ros_time_sm_starttm;
+ros::Time ros_time_sm_stoptm;
+ros::Time sync_time_test;
+mjtNum *torque_mj;
+
+// user state for pub
+
+float com_time;
+float dif_time;
+
+//void c_pause();
+//void c_slowmotion();
+void c_reset();
+
+//---------------------callback functions --------------------------------
+
+void jointset_callback(const mujoco_ros_msgs::JointSetConstPtr &msg);
+void sim_command_callback(const std_msgs::StringConstPtr &msg);
+void state_publisher_init();
+void state_publisher();
+void mujoco_ros_connector_init();
+void mycontroller(const mjModel *m, mjData *d);
+
+#endif
---
+++ mujoco_2_ros/src/main.cpp
@@ -0,0 +1,197 @@
+/*  Copyright © 2018, Roboti LLC
+
+    This file is licensed under the MuJoCo Resource License (the "License").
+    You may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.roboti.us/resourcelicense.txt
+*/
+
+#include "mjros.h"
+
+// drop file callback
+void drop(GLFWwindow *window, int count, const char **paths)
+{
+    // make sure list is non-empty
+    if (count > 0)
+    {
+        mju_strncpy(filename, paths[0], 1000);
+        settings.loadrequest = 1;
+        ROS_INFO("DROP REQUEST");
+    }
+}
+
+// load mjb or xml model
+void loadmodel(void)
+{
+    // clear request
+    settings.loadrequest = 0;
+
+    // make sure filename is not empty
+    if (!filename[0])
+        return;
+
+    // load and compile
+    char error[500] = "";
+    mjModel *mnew = 0;
+    if (strlen(filename) > 4 && !strcmp(filename + strlen(filename) - 4, ".mjb"))
+    {
+        mnew = mj_loadModel(filename, NULL);
+        if (!mnew)
+            strcpy(error, "could not load binary model");
+    }
+    else
+    {
+        mnew = mj_loadXML(filename, NULL, error, 500);
+    }
+    if (!mnew)
+    {
+        printf("%s\n", error);
+        return;
+    }
+
+    // compiler warning: print and pause
+    if (error[0])
+    {
+        // mj_forward() below will print the warning message
+        printf("Model compiled, but simulation warning (paused):\n  %s\n\n",
+               error);
+        settings.run = 0;
+    }
+
+    // delete old model, assign new
+    mj_deleteData(d);
+    mj_deleteModel(m);
+    m = mnew;
+    d = mj_makeData(m);
+
+    int i = settings.key;
+    d->time = m->key_time[i];
+    mju_copy(d->qpos, m->key_qpos + i * m->nq, m->nq);
+    mju_copy(d->qvel, m->key_qvel + i * m->nv, m->nv);
+    mju_copy(d->act, m->key_act + i * m->na, m->na);
+
+    mj_forward(m, d);
+
+    ros_sim_started = true;
+    torque_mj = mj_stackAlloc(d, (int)m->nu);
+
+    // re-create scene and context
+    mjv_makeScene(m, &scn, maxgeom);
+    mjr_makeContext(m, &con, 50 * (settings.font + 1));
+
+    // clear perturbation state
+    pert.active = 0;
+    pert.select = 0;
+    pert.skinselect = -1;
+
+    // align and scale view, update scene
+    alignscale();
+    mjv_updateScene(m, d, &vopt, &pert, &cam, mjCAT_ALL, &scn);
+
+    // set window title to model name
+    if (window && m->names)
+    {
+        char title[200] = "Simulate : ";
+        strcat(title, m->names);
+        glfwSetWindowTitle(window, title);
+    }
+
+    // rebuild UI sections
+    makesections();
+
+    // full ui update
+    uiModify(window, &ui0, &uistate, &con);
+    uiModify(window, &ui1, &uistate, &con);
+    updatesettings();
+
+    mujoco_ros_connector_init();
+    std::cout << " MODEL LOADED " << std::endl;
+}
+// run event loop
+int main(int argc, char **argv)
+{
+    // :: ROS CUSTUM :: initialize ros
+    ros::init(argc, argv, "mujoco_ros");
+    ros::NodeHandle nh("~");
+
+    std::string key_file;
+    nh.param<std::string>("license", key_file, "mjkey.txt");
+
+    //register publisher & subscriber
+    joint_state_pub = nh.advertise<sensor_msgs::JointState>("/mujoco_ros_interface/joint_states", 1);
+    sim_time_pub = nh.advertise<std_msgs::Float32>("/mujoco_ros_interface/sim_time", 1);
+    sensor_state_pub = nh.advertise<mujoco_ros_msgs::SensorState>("/mujoco_ros_interface/sensor_states", 1);
+    joint_set = nh.subscribe<mujoco_ros_msgs::JointSet>("/mujoco_ros_interface/joint_set", 1, jointset_callback, ros::TransportHints().tcpNoDelay(true));
+    //joint_set_mujoco = nh.subscribe<mujoco_ros_msgs::JointSet>("/mujoco_ros_interface/joint_set_mujoco",1,joint)
+    sim_command_sub = nh.subscribe<std_msgs::String>("/mujoco_ros_interface/sim_command_con2sim", 100, sim_command_callback);
+    sim_command_pub = nh.advertise<std_msgs::String>("/mujoco_ros_interface/sim_command_sim2con", 1);
+
+    ROS_INFO("ROS initialize complete");
+
+    // initialize everything
+    init(key_file);
+
+    std::string model_file;
+    // request loadmodel if file given (otherwise drag-and-drop)
+    if (nh.getParam("model_file", model_file))
+    {
+        mju_strncpy(filename, model_file.c_str(), 1000);
+        settings.loadrequest = 2;
+        ROS_INFO("model is at %s", model_file.c_str());
+    }
+
+    // start simulation thread
+    std::thread simthread(simulate);
+
+    // event loop
+    while ((!glfwWindowShouldClose(window) && !settings.exitrequest) && ros::ok())
+    {
+        // start exclusive access (block simulation thread)
+        mtx.lock();
+        // load model (not on first pass, to show "loading" label)
+        if (settings.loadrequest == 1)
+        {
+            ROS_INFO("Load Request");
+            loadmodel();
+        }
+        else if (settings.loadrequest > 1)
+            settings.loadrequest = 1;
+
+        // handle events (calls all callbacks)
+        glfwPollEvents();
+
+        // prepare to render
+        prepare();
+
+        // ros events
+        rosPollEvents();
+
+        // end exclusive access (allow simulation thread to run)
+        mtx.unlock();
+
+        // render while simulation is running
+        render(window);
+    }
+
+    // stop simulation thread
+    settings.exitrequest = 1;
+    simthread.join();
+
+    // delete everything we allocated
+    uiClearCallback(window);
+    mj_deleteData(d);
+    mj_deleteModel(m);
+    mjv_freeScene(&scn);
+    mjr_freeContext(&con);
+
+    // deactive MuJoCo
+    mj_deactivate();
+
+// terminate GLFW (crashes with Linux NVidia drivers)
+#if defined(__APPLE__) || defined(_WIN32)
+    glfwTerminate();
+#endif
+
+    return 0;
+}
